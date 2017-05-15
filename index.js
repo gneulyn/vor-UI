331249216e0702fb22dd4dcbd9466686631fc2e6
lukasIO <lukas.seiler@zoho.com>
2017-05-16 00:40:12 +0200

    colorwheel working, callback throttling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var socket = new WebSocket("ws://192.168.178.113:8006");
+var socket = new WebSocket("ws://127.0.0.1:8006");
 
 var body = document.getElementById('wrapper');
 
+var cwSize = 400;
+
+/*
 var redSlider = document.createElement("INPUT");
 redSlider.setAttribute("type", "range");
 redSlider.setAttribute("id", "red");
@@ -26,39 +29,93 @@ blueSlider.setAttribute("max", "255");
 var nextButton = document.createElement("BUTTON");
 nextButton.setAttribute("id", "next");
 
-var picker =
+*/
+
+
 
 
 var green = 0;
 var blue = 0;
 
+//close socket properly if page is closed/refreshed
+document.onunload = function (){
+    socket.close();
+};
+
+//function being called once when socket is opened (connected)
+socket.onopen = function(){
+    console.log("connected");
+    socket.send("ask for current color here?");
+};
+
+//callback for receiving messages from the server
+socket.onmessage = function(event) {
+  var f = document.getElementById("chatbox").contentDocument;
+  var text = "";
+  var servermsg = JSON.parse(event.data);
+};
+
+
+
+
+
 window.onload = function () {
 
-    var update = function() {
+    //create the colorwheel (alter war das ne ficke mit der library, bis das lief)
+    var cw = Raphael.colorwheel(document.getElementById("colorwheel"),cwSize);
 
+    //after initialising the colorwheel, the "default" color can be set, would make sense to get the current color from the websocket for this
+    cw.color("#F00");
+
+        
+    //throttle the callback funtion to make sure it only sends maximum every 50 ms, but also doesnt send if it doesnt need to
+    cw.onchange ( throttle(function(cwColor){
         var msg = {
-            red: document.getElementById("red").value,
-            green: document.getElementById("green").value,
-            blue: document.getElementById("blue").value
+            ledcolor: {
+                red: cwColor.r,
+                green: cwColor.g,
+                blue: cwColor.b
+            }
         };
 
+        socket.send(JSON.stringify(msg));
+    },50));
 
-        if (socket.readyState == 1) {
-            //socket.send(JSON.stringify(msg));
-            console.log(msg);
-        } else {
-            console.log(socket);
-        }
 
-    };
-
-    //document.getElementById("red").onmousedown = setInterval(update, 1000);
+       
 };
+
+
 
 
 
 function updateValue(value, id) {
 
+}
+
+
+//from https://remysharp.com/2010/07/21/throttling-function-calls
+function throttle(fn, threshhold, scope) {
+  threshhold || (threshhold = 250);
+  var last,
+      deferTimer;
+  return function () {
+    var context = scope || this;
+
+    var now = +new Date,
+        args = arguments;
+    if (last && now < last + threshhold) {
+      // hold on to it
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        last = now;
+        fn.apply(context, args);
+      }, threshhold);
+    } else {
+      last = now;
+      fn.apply(context, args);
+    }
+  };
 }
 
 
@@ -69,12 +126,12 @@ function updateValue(value, id) {
 //window.setInterval(updateValue, 100);
 
 
-
+/*
 body.appendChild(redSlider);
 body.appendChild(greenSlider);
 body.appendChild(blueSlider);
 body.appendChild(nextButton);
 
 
-
+*/
 
